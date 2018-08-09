715cf2edaf84b4b86aeaa7b262154e8c4c28362c
Kenneth <rekerc4@gmail.com>
2018-08-09 18:50:11 -0400

    Second commit

--- scripts/ctrl.js
+++ scripts/ctrl.js
@@ -4,7 +4,7 @@
 function TodoController(){
     let vm = this; 
     vm.arr = [{text: "Do laundry", completed: true}, 
-    {text: "Make Dinner", completed: true}, 
+    {text: "Make Dinner", completed: false}, 
     {text: "Pay Bills", completed: true}, 
     {text: "Pick Up Kids", completed: false}]
 }
