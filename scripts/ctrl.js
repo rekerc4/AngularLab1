5a6e2b78dfbf6cfafc4e96fbd14baacb47798239
Kenneth <rekerc4@gmail.com>
2018-08-13 16:54:59 -0400

    Part 2

--- scripts/ctrl.js
+++ scripts/ctrl.js
@@ -1,13 +1,35 @@
 "use strict";
 
 
+
 function TodoController(){
     let vm = this; 
     vm.arr = [{text: "Do laundry", completed: true}, 
     {text: "Make Dinner", completed: false}, 
     {text: "Pay Bills", completed: true}, 
     {text: "Pick Up Kids", completed: false}]
+    vm.addTask = (newTask) => {
+        console.log(newTask.newTask);
+        if(!newTask.newTask){
+            return; 
+        }
+        else{
+        console.log(typeof newTask.newTask.text);
+        vm.arr.push({text: angular.copy(newTask.newTask.text), completed: false});
+        console.log(vm.arr);
+        }
+
+    }
+    vm.deleteTask = (index) =>{
+        console.log(index);
+        vm.arr.splice(index, 1); 
+    }
+    vm.completeTask = (index) => {
+        vm.arr[index].completed = true; 
+    }
 }
+
+
 
 
 
